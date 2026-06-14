0add159499ec78b0289efe3ce125009a4ab54ec9
pantakamal7-cyber <pantakamal7@gmail.com>
2026-06-14 19:03:34 +1000

    Update supabaseClient.js

--- client/src/supabaseClient.js
+++ client/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://xateuyrdodkjwmheqych.supabase.co'
-const supabaseAnonKey = 'PASTE_YOUR_LONG_ANON_KEY_HERE'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhdGV1eXJkb2RrandtaGVxeWNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzODc0NjEsImV4cCI6MjA5Njk2MzQ2MX0.7tKoGcFwK5dsqHbuDI1_AM4nqbw1WhorLebnBnioQLY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
